56e3e663669ab1df07628ca0e8584991938781a9
Daniel Vang <danielvang@ghalliance.org>
2016-01-20 17:36:39 -0600

    added method in scissors class, but still needs editing

--- rockPaperScissors/rockPaperScissors/Scissors.cs
+++ rockPaperScissors/rockPaperScissors/Scissors.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace rockPaperScissors
 {
+    
     public class Scissors
     {
+        int scissors;
+        int paper;
+        int rock;
 
+    public void winCondition()
+        {
+            if (this.scissors == this.scissors)
+            {
+                Console.WriteLine("Draw");
+
+            }
+            else if (this.scissors > this.paper)
+            {
+                Console.WriteLine("Victory");
+            }
+            else if(this.scissors <this.rock)
+            {
+                Console.WriteLine("Loss");
+            }
+        }
     }
 }
